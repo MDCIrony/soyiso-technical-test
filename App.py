267d5100c9094c7ef4ea315d9ea01cbcc04b4512
MigueldCast <miguel.acastillodiaz@outlook.es>
2023-02-21 17:57:00 -0500

    Organizando estructura stock_manager/ y App.py

---
+++ App.py
@@ -0,0 +1,8 @@
+# Description: This is the main file for the stock manager application
+# Author: Miguel Alejandro Díaz Castillo
+import sys
+
+import stock_manager.main as Stock
+
+if __name__ == '__main__':
+    Stock.Hello()
